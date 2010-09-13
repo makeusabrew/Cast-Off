519e6b2e15961c634fd46896a7f514887c9493f5
Nick Payne <nick@kurai.co.uk>
2010-09-13 20:25:24 +0100

    modifying index route to read html file

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 var http = require("http");
 var url = require("url");
+var path = require("path");
+var fs = require("fs");
 
 http.createServer(function(request, response) {
     var cUrl = url.parse(request.url).pathname;
     switch (cUrl) {
         case '/':
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.write("Hello World");
-            response.end();
+            fs.readFile(process.cwd()+"/index.html", function(err, file) {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.write(file, "binary");
+                response.end();
+            });
             break;
         default:
             console.log("sending 404 for URL", cUrl);
             response.writeHead(404, {'Content-Type':'text/html'});
             response.write("Sorry, that page could not be found");
             response.end();
-        break;
+            break;
     }
 }).listen(8124);
 
